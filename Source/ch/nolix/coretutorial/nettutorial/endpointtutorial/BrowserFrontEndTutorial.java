4d652f85b358a01c67f3eebd6d01b790bcd8bd17
Nimeon <silvan.wyss@gmail.com>
2023-11-24 10:17:10 +0100

    edited BrowserFrontEndTutorial

--- Source/ch/nolix/coretutorial/nettutorial/endpointtutorial/BrowserFrontEndTutorial.java
+++ Source/ch/nolix/coretutorial/nettutorial/endpointtutorial/BrowserFrontEndTutorial.java
@@ -9,17 +9,16 @@ public final class BrowserFrontEndTutorial {
   private BrowserFrontEndTutorial() {
   }
 
-  @SuppressWarnings("resource")
   public static void main(String[] args) {
 
     //Creates a Server.
-    final var server = Server.forHttpPort();
+    try (final var server = Server.forHttpPort()) {
 
-    //Starts a web browser that will connect to the Server.
-    ShellProvider.startDefaultWebBrowserOpeningLoopBackAddress();
+      //Starts a web browser that will connect to the Server.
+      ShellProvider.startDefaultWebBrowserOpeningLoopBackAddress();
 
-    //Closes the Server after 2 seconds.
-    GlobalSequencer.waitForSeconds(2);
-    server.close();
+      //Closes the Server after 2 seconds.
+      GlobalSequencer.waitForSeconds(2);
+    }
   }
 }
