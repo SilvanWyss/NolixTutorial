881536aff16a83894ebffff9d533a8db8856983e
Silvan Wyss <silvan.wyss@gmail.com>
2024-12-07 16:09:29 +0100

    Create package info file for the applicationtutorial package

---
+++ Source/ch/nolix/applicationtutorial/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * The {@link ch.nolix.applicationtutorial} package defines tutorials for the
+ * {@link ch.nolix.application} package.
+ * 
+ * @author Silvan Wyss
+ * @date 2024-12-07
+ */
+package ch.nolix.applicationtutorial;
