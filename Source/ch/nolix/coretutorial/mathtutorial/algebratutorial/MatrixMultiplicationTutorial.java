640982a53750ec0b043a07d0fd07d7c12a415187
Silvan Wyss <silvan.wyss@gmail.com>
2024-12-07 19:41:23 +0100

    Update MatrixMultiplicationTutorial

--- Source/ch/nolix/coretutorial/mathtutorial/algebratutorial/MatrixMultiplicationTutorial.java
+++ Source/ch/nolix/coretutorial/mathtutorial/algebratutorial/MatrixMultiplicationTutorial.java
@@ -11,22 +11,17 @@ public final class MatrixMultiplicationTutorial {
   public static void main(String[] args) {
 
     //Creates matrix1.
-    final var matrix1 = new Matrix(3, 3)
-      .setValues(2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 1.0, 2.0, 3.0);
+    final var matrix1 = //
+    Matrix.createMatrixWithRowCountAndColumnCount(3, 3).setValues(2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 1.0, 2.0, 3.0);
 
     //Creates matrix2.
-    final var matrix2 = new Matrix(3, 3)
-      .setValues(4.0, 5.0, 6.0, 1.0, 2.0, 3.0, 2.0, 3.0, 4.0);
+    final var matrix2 = //
+    Matrix.createMatrixWithRowCountAndColumnCount(3, 3).setValues(4.0, 5.0, 6.0, 1.0, 2.0, 3.0, 2.0, 3.0, 4.0);
 
-    //Calculates the product of matrix1 and matrix2.
-    final var product = matrix1.getProduct(matrix2);
+    //Calculates the productMatrix of matrix1 and matrix2.
+    final var productMatrix = matrix1.getProduct(matrix2);
 
-    //Logs the console matrix1, matrix2 and the product.
-    GlobalLogger.logInfo(
-      matrix1.toString()
-      + " * "
-      + matrix2.toString()
-      + " = "
-      + product.toString());
+    //Logs matrix1, matrix2 and the productMatrix.
+    GlobalLogger.logInfo(matrix1 + " * " + matrix2 + " = " + productMatrix);
   }
 }
