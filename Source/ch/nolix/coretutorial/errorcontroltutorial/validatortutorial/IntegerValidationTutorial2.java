bfbb7527f888c8d2e42ad321651c0b6dc235b1bc
Silvan Wyss <silvan.wyss@gmail.com>
2024-12-07 17:42:02 +0100

    Update IntegerValidationTutorial2

--- Source/ch/nolix/coretutorial/errorcontroltutorial/validatortutorial/IntegerValidationTutorial2.java
+++ Source/ch/nolix/coretutorial/errorcontroltutorial/validatortutorial/IntegerValidationTutorial2.java
@@ -2,36 +2,17 @@ package ch.nolix.coretutorial.errorcontroltutorial.validatortutorial;
 
 import ch.nolix.core.errorcontrol.validator.GlobalValidator;
 
-/**
- * This class is a tutorial how the zeta validator validates an integer.
- * 
- * @author Silvan Wyss
- * @date 2016-12-01
- */
 public final class IntegerValidationTutorial2 {
 
-  /**
-   * Prevents that an instance of the {@link IntegerValidationTutorial2} can be
-   * created.
-   */
   private IntegerValidationTutorial2() {
   }
 
-  /**
-   * Lets the zeta validator suppose once that 5 is positive and once suppose that
-   * 5 is negative. This is done that the error message of a probable thrown
-   * exception contains the variable name.
-   * 
-   * @param args
-   */
   public static void main(String[] args) {
 
-    //Supposes that 5 is positive, what makes that the zeta validator does not
-    //complain.
+    //Lets the GlobalValidator assert that 5 is positive, what will not result in any complain.
     GlobalValidator.assertThat(5).thatIsNamed("size").isPositive();
 
-    //Supposes that 5 is negative, what makes that the zeta validator throws a
-    //NegativeArgumentException.
+    //Lets the GlobalValidator assert that 5 is negative, what will result in a NegativeArgumentException.
     GlobalValidator.assertThat(5).thatIsNamed("size").isNegative();
   }
 }
