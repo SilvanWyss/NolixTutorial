8c8c125f8efa0b911c2ba74043ceb68096550f2a
Silvan Wyss <silvan.wyss@gmail.com>
2025-06-28 10:58:15 +0200

    Edit AsSoonAsNoMoreMethodTutorial

--- Source/ch/nolix/core/programcontrol/flowcontrol/AsSoonAsNoMoreMethodTutorial.java
+++ Source/ch/nolix/core/programcontrol/flowcontrol/AsSoonAsNoMoreMethodTutorial.java
@@ -5,17 +5,17 @@ import ch.nolix.core.errorcontrol.logging.Logger;
 
 final class AsSoonAsNoMoreMethodTutorial {
 
-  private AsSoonAsNoMoreMethodTutorial() {
-  }
-
   public static void main(String[] args) {
 
     final var cats = ArrayList.withElement("Garfield", "Simba", "Smokey");
 
     FlowController
       .asSoonAsNoMore(cats::containsAny)
-      .runInBackground(() -> Logger.logInfo("Couch is not scratched anymore!"));
+      .runInBackground(() -> Logger.logInfo("The couch is not scratched anymore!"));
 
     cats.clear();
+  }
+
+  private AsSoonAsNoMoreMethodTutorial() {
   }
 }
