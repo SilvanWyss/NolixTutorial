bd0141937263d2bb746a7cefd110cd248760ec9e
Silvan Wyss <silvan.wyss@gmail.com>
2025-06-28 10:12:01 +0200

    Edit RunningJarTutorial

--- Source/ch/nolix/core/environment/runningjar/RunningJarTutorial.java
+++ Source/ch/nolix/core/environment/runningjar/RunningJarTutorial.java
@@ -4,19 +4,18 @@ import ch.nolix.core.errorcontrol.logging.Logger;
 
 final class RunningJarTutorial {
 
-  private RunningJarTutorial() {
-  }
-
   public static void main(String[] args) {
 
     //Defines resourcePath.
-    final var resourcePath = //
-    "text/willkommen_und_abschied.txt";
+    final var resourcePath = "text/willkommen_und_abschied.txt";
 
     //Gets the resource from the resourcePath.
     final var resource = RunningJar.getResource(resourcePath);
 
     //Logs the resource.
     Logger.logInfo(resource);
+  }
+
+  private RunningJarTutorial() {
   }
 }
