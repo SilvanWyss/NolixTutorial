1f18139b7b4473f6f190878c3445f2ac4c2b273a
Silvan Wyss <silvan.wyss@gmail.com>
2025-06-29 17:13:54 +0200

    Create PartialRefreshOnSslServerTutorial

---
+++ Source/ch/nolix/system/webgui/main/PartialRefreshOnSslServerTutorial.java
@@ -0,0 +1,20 @@
+package ch.nolix.system.webgui.main;
+
+import ch.nolix.system.application.main.SslServer;
+
+public final class PartialRefreshOnSslServerTutorial {
+
+  public static void main(String[] args) {
+
+    //Creates a SslServer.
+    final var sslServer = SslServer.forHttpsPortAndDomainAndSSLCertificateFromNolixConfiguration();
+
+    //Adds a default Application to the SslServer.
+    sslServer.addApplicationWithNameAndInitialSessionClassAndVoidContext(
+      "Partial refresh tutorial",
+      PartialRefreshTutorial.Session.class);
+  }
+
+  private PartialRefreshOnSslServerTutorial() {
+  }
+}
