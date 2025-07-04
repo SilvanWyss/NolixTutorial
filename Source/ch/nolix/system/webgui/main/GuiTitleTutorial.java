c744565e20c0d2659fd5035dfacfc0784d33951c
Silvan Wyss <silvan.wyss@gmail.com>
2025-07-04 14:40:10 +0200

    Create GuiTitleTutorial

---
+++ Source/ch/nolix/system/webgui/main/GuiTitleTutorial.java
@@ -0,0 +1,41 @@
+package ch.nolix.system.webgui.main;
+
+import ch.nolix.core.environment.localcomputer.ShellProvider;
+import ch.nolix.core.programcontrol.flowcontrol.FlowController;
+import ch.nolix.system.application.main.Server;
+import ch.nolix.system.application.webapplication.WebClientSession;
+
+final class GuiTitleTutorial {
+
+  public static void main(String[] args) {
+
+    //Creates a Server.
+    final var server = Server.forHttpPort();
+
+    //Adds a default Application to the Server.
+    server.addDefaultApplicationWithNameAndInitialSessionClassAndVoidContext("GUI title tutorial", Session.class);
+
+    //Starts a web browser that will connect to the Server.
+    ShellProvider.startDefaultWebBrowserOpeningLoopBackAddress();
+
+    //Closes the Server as soon as it does not have a client connected any more.
+    FlowController
+      .waitForSeconds(2)
+      .andThen()
+      .asSoonAsNoMore(server::hasClientConnected)
+      .runInBackground(server::close);
+  }
+
+  private static final class Session extends WebClientSession<Object> {
+
+    @Override
+    protected void initialize() {
+
+      //Sets the title of the GUI of the current Session.
+      getStoredGui().setTitle("---Demo---");
+    }
+  }
+
+  private GuiTitleTutorial() {
+  }
+}
